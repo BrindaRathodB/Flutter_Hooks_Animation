aeca6299729e78514f2be0cc2d70e3282b676d5c
Abhishek <abhishek.doshi@bacancy.com>
2021-09-27 16:38:25 +0530

    home screen ui changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,29 +6,13 @@ import 'package:flutter_hooks_demo/hooks/controller_for_animation.dart';
 class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final hideFabAnimController = useAnimationController(duration: kThemeAnimationDuration, initialValue: 1);
-    // final scrollController = useScrollControllerForAnimation(hideFabAnimController);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Let's Scroll"),
+        leading:Icon(Icons.home),
+        title: Text("Home"),
       ),
-      floatingActionButton: FadeTransition(
-        opacity: hideFabAnimController,
-        child: ScaleTransition(
-          scale: hideFabAnimController,
-          child: FloatingActionButton.extended(
-            label: const Text('Useless Floating Action Button'),
-            onPressed: () {},
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // body: ListView(
-      //   controller: scrollController,
-      //   children: <Widget>[
-      //     for (int i = 0; i < 5; i++) Card(child: FittedBox(child: FlutterLogo())),
-      //   ],
-      // ),
+      body: Center(child: Text("Welcome to Home screen!")),
+      
     );
   }
 }
